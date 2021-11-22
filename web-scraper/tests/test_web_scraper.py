67a5df14ffae828419f4bd36b0b407b6c152934e
mohammedalsamki <momosamak21@gmail.com>
2021-11-22 18:51:35 -0500

    solve lab17

---
+++ web-scraper/tests/test_web_scraper.py
@@ -0,0 +1,27 @@
+from web_scraper.scraper import (get_citations_needed_count,
+                                  get_citations_needed_report)
+
+
+def test_successfully_return_citations_needed_count():
+    # Arrange
+    my_url = 'https://en.wikipedia.org/wiki/History_of_Mexico'
+    # Actual
+    actual = get_citations_needed_count(my_url)
+    # Expected
+    excepted = 5
+    # Assert
+    assert actual == excepted
+
+def test_successfully_return_citations_needed_report():
+    # Arrange
+    my_url = 'https://en.wikipedia.org/wiki/History_of_Mexico'
+    # Actual
+    actual = get_citations_needed_report(my_url)
+    # Expected
+    text = open('expected.txt', 'r')
+    excepted = text.read()
+    assert "".join(actual) == excepted
+
+
+
+
